dd75b995272dc91d2949c722dc6206b68582aa2b
fabrik12 <fbalarezo@unsa.edu.pe>
2025-04-10 17:36:34 -0500

    Se agrego una funcion para verificar si es un archivo de imagen o no

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-import os
+import os, imghdr
 from flask import render_template, request, redirect, url_for, abort
 from werkzeug.utils import secure_filename
 from app import app
+
+def validate_image(stream):
+    header = stream.read(512)
+    stream.seek(0)
+    format = imghdr.what(None, header)
+    if not format:
+        return None
+    return '.' + (format if format != 'jpeg' else 'jpg')
 
 @app.route('/')
 def index():
@@ -16,7 +24,8 @@ def upload_file():
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
-        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
+                file_ext != validate_image(uploaded_file.stream):
             abort(400)
         uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return redirect(url_for('index'))
