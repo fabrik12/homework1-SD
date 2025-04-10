576b275a67e8c643931714804b5182ba0883f048
fabrik12 <fbalarezo@unsa.edu.pe>
2025-04-10 16:28:02 -0500

    Formulario para subir un archivo (simple sin comprobaciones de seguridad)

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+
+UPLOAD_FOLDER = './app/uploads'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
-from flask import render_template
+import os
+from flask import render_template, request, redirect, url_for
 from app import app
 
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename))
+    return redirect(url_for('index'))
