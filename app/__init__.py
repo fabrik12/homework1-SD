1854fe59d0b3d77aefe91d907819222891ca699c
fabrik12 <fbalarezo@unsa.edu.pe>
2025-04-10 17:28:06 -0500

    Pruebas de longitud de contenido y verificacion de extension: works!

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,7 @@ UPLOAD_FOLDER = './app/uploads'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024*1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpeg', '.jpg', '.png', '.gif']
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 import os
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
 from app import app
 
 @app.route('/')
@@ -9,6 +10,13 @@ def index():
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename))
+    # Prueba de seguridad 
+    # uploaded_file.filename = "../../.bashrc"
+    # print("Viene por aqui")
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     return redirect(url_for('index'))
