5a3e22fedf24ba4bb1cc5ac9f3efe4ba25214c40
fabrik12 <fbalarezo@unsa.edu.pe>
2025-04-10 20:03:29 -0500

    Se incluye manejo de errores con Dropzone y en el server, solucion al error de pasar imagenes jpg validas que cambia el formato a jpeg

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,18 @@ import os, imghdr
 from flask import render_template, request, redirect, url_for, abort, \
         send_from_directory
 from werkzeug.utils import secure_filename
+from PIL import Image
 from app import app
 
-def validate_image(stream):
+def validate_image(stream, file_ext):
     try:
-        header = stream.read(1024)
-        stream.seek(0)
-        format = imghdr.what(None, header)
-        if not format:
-            return None
-        return '.' + format
+        img = Image.open(stream)
+        img.verify()
+        print(f"Este el formato detectado: {img.format.lower()}")
+        if file_ext=='.jpg' and img.format.lower()=="jpeg":
+            #Mantener la extension .jpg si el formato detectado es valido y retorna como jpeg
+            return file_ext
+        return '.' + img.format.lower() 
     except Exception as e:
         print(f"Error al validar la imagen: {e}")
         return None
@@ -21,7 +23,7 @@ def index():
     files = os.listdir("app/" + app.config['UPLOAD_FOLDER'])
     return render_template('index.html', files=files)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
     # Prueba de seguridad 
@@ -30,14 +32,17 @@ def upload_file():
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
+        print(file_ext)
         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
-                file_ext != validate_image(uploaded_file.stream):
-            abort(400)
+                file_ext != validate_image(uploaded_file.stream, file_ext):
+            return "Invalid image", 400
         uploaded_file.save(os.path.join("app/"+ app.config['UPLOAD_FOLDER'], filename))
-    return redirect(url_for('index'))
+    return '', 204
 
 @app.route('/uploads/<filename>')
 def upload(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
